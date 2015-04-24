ce5e8140ba7bc4a9849aa7ff13e487800c1f2339
Brian Peterson <bepetersn@gmail.com>
2015-04-23 21:15:46 -0500

    Add requirements.txt and update version to include tests

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name = "gmail-client",
-    version = "0.0.7.7",
+    version = "0.0.7.8",
     author = "Wilberto Morales",
     author_email = "wilbertomorales777@gmail.com",
     description = ("A Pythonic interface for Google Mail. Based of https://github.com/charlierguo/gmai"),
     license = "MIT",
     keywords = "google gmail client",
     url = "https://github.com/wilbertom/gmail",
-    packages=['gmail_client', 'gmail_client.codecs'],
+    packages=['gmail_client', 'gmail_client.codecs', 'tests'],
     classifiers=[
         "Topic :: Communications :: Email",
         "License :: OSI Approved :: MIT License",
