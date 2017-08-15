2c8a2596b61de4994dd414591fd184d863d96448
Brian Peterson <bepetersn@gmail.com>
2017-08-15 13:20:07 -0600

    Bump to version 0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "gmail-client",
-    version = "0.0.7.7",
+    version = "0.0.8.0",
     author = "Wilberto Morales",
     author_email = "wilbertomorales777@gmail.com",
     description = ("A Pythonic interface for Google Mail. Based of https://github.com/charlierguo/gmai"),
